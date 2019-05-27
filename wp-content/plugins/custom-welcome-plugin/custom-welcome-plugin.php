8774668e63059595327728e0f751276f582d0846
Lennert Slabbinck <LennertSk@users.noreply.github.com>
2019-05-27 14:01:09 +0200

    Wordpress added

---
+++ wp-content/plugins/custom-welcome-plugin/custom-welcome-plugin.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Plugin Name: Custom Welcome Plugin
+ * Plugin URI: https://www.the90sbutton.com/
+ * Description: Custom welcome plugin
+ * Version: 1.0
+ * Author: Lennert Slabbinck
+ * Author URI: https://www.the90sbutton.com/
+ */
+
+
+// if (!isset($_COOKIE['visited'])) 
+// {
+//     setcookie ('visited', 'yes', time() + 3600); 
+//     $cookie = ++$_COOKIE['count'];
+//     setcookie('count', $cookie);
+// }
+// else{
+// 	setcookie ('count', 1);
+// }
+
+setcookie('count', isset($_COOKIE['count']) ? ++$_COOKIE['count'] : 1);
+
+
+
+// Register the widget ----------------------------------------------
+
+add_action( 'widgets_init', 'src_load_widgets' );
+
+function src_load_widgets() {
+    register_widget( 'My_Custom_Widget');
+}
+
+// Widget 01 ----------------------------------------------
+class My_Custom_Widget extends WP_Widget {
+
+    public function __construct() {
+    // Define the constructor
+
+	    $options = array(
+	        'classname' => 'custom_hello_widget',
+	        'description' => 'Custom hello widget',
+	    );
+
+	    parent::__construct(
+	        'custom_hello_widget', 'Custom hello widget', $options
+	    );
+	}
+
+    public function widget( $args, $instance ) {
+    // Keep this line
+	    echo $args['before_widget'];
+
+	    // echo $args['before_title'] . apply_filters( 'widget_title', 'Custom Hello Widget' ) . $args['after_title'];
+
+
+	    // if (!isset($_COOKIE['visited'])) {
+     // 		echo "Welcome, looks like this is your first visit here, feel free to look around!";     
+    	// }
+    	// else{
+    	// 	echo "Welcome back, check out my latest projects!";
+    	// }
+    	// echo "Hellow, this is your " .$_COOKIE('count');
+
+    	if ($_COOKIE['count'] == 0): 
+            echo "Welcome! This is your visit! So sit back and enjoy the ride!.";
+        else:
+            echo "Welcome back! You have viewed this site " .$_COOKIE['count']."  times."; 
+        endif;
+
+
+	    // Keep this line
+	    echo $args['after_widget'];
+	}
+
+}
+
+
+
+
+?> 
