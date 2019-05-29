d407f158a11a55d34894c8707ebe62a023b0bba9
Lennert Slabbinck <LennertSk@users.noreply.github.com>
2019-05-29 19:37:35 +0200

    Employment plugin

---
+++ wp-content/plugins/custom-employment-plugin/custom-employment-plugin.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Plugin Name: Custom Employment Plugin
+ * Plugin URI: https://www.the90sbutton.com/
+ * Description: Custom Employment plugin
+ * Version: 1.0
+ * Author: Lennert Slabbinck
+ * Author URI: https://www.the90sbutton.com/
+ */
+
+
+
+// Widget 01 ----------------------------------------------
+
+class Custom_Employment_Widget extends WP_Widget {
+    // Initialize Widget with Options
+    public function __construct() {
+        parent::__construct(
+            'custom_employment_widget',
+            'Custom Emplyment Widget',
+            array(
+                'classname'   => 'custom_employment_widget',
+                'description' => 'Custom Emplyment Widget'
+            )
+        );
+    }
+
+    // Widget Front End
+    public function widget( $args, $instance ) {
+        extract( $args );
+        extract( $instance );
+
+        echo $before_widget;
+        /* Widget Content Below */
+            echo "Right now I'm  "; 
+        /* Widget Content Above */
+        echo $after_widget;
+    }
+
+    // Widget Admin Form
+    public function form( $instance ) { ?>
+        <?php extract( $instance ); ?>
+        <p>
+            <label>
+                <input type="radio" value="student" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'student' ); ?> id="<?php echo $this->get_field_id( 'job' ); ?>" />
+                <?php esc_attr_e( 'studying hard for my exams', 'text_domain' ); ?>
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="radio" value="working" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'working' ); ?> id="<?php echo $this->get_field_id( 'jos' ); ?>" />
+                <?php esc_attr_e( 'working my ass off 24/7', 'text_domain' ); ?>
+            </label>
+        </p>
+        <p>
+            <label>
+                <input type="radio" value="jobless" name="<?php echo $this->get_field_name( 'job' ); ?>" <?php checked( $job, 'jobless' ); ?> id="<?php echo $this->get_field_id( 'jos' ); ?>" />
+                <?php esc_attr_e( 'not really doing anything, just enjoying life', 'text_domain' ); ?>
+            </label>
+        </p>
+    <?php }
+
+    // Sanitize and Save Options
+    public function update( $new_instance, $old_instance ) {
+        extract( $new_instance );
+        $instance = array();
+
+        $instance['job'] = ( !empty( $job ) ) ? sanitize_text_field( $job ) : null;
+
+        return $instance;
+    }
+}
+
+function custom_employment_widget() {
+    register_widget( 'Custom_Employment_Widget' );
+}
+add_action( 'widgets_init', 'custom_employment_widget' );
+
+
+
+?> 
+
+
--- wp-content/themes/hamilton/functions.php
+++ wp-content/themes/hamilton/functions.php
@@ -30,62 +30,7 @@ function sidebar_widgets_init_2() {
 add_action( 'widgets_init', 'sidebar_widgets_init_2' );
 
 
-/* widgetz */
 
-function custom_register_widget() {
-register_widget( 'employment_widget' );
-}
-
-add_action( 'widgets_init', 'custom_register_widget' );
-
-class employment_widget extends WP_Widget {
-
-function __construct() {
-parent::__construct(
-// widget ID
-'employment_widget',
-// widget name
-__('Employment Custom Widget', ' employment_widget_domain'),
-// widget description
-array( 'description' => __( 'Employment Custom Widget', 'employment_widget_domain' ), )
-);
-}
-public function widget( $args, $instance ) {
-$title = apply_filters( 'widget_title', $instance['title'] );
-echo $args['before_widget'];
-//if title is present
-if ( ! empty( $title ) )
-	//output
-echo __( 'Current state of employment:  ', 'employment_widget_domain' );
-echo $args['before_title'] . $title . $args['after_title'];
-
-echo $args['after_widget'];
-}
-public function form( $instance ) {
-if ( isset( $instance[ 'title' ] ) )
-$title = $instance[ 'title' ];
-else
-$title = __( 'Default Title', 'employment_widget_domain' ); 
-?>
-<p>
-<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Current employment:' ); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-
-
-</p>
-
-
-
-
-<?php
-}
-public function update( $new_instance, $old_instance ) {
-$instance = array();
-$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-return $instance;
-}
-
-}
 
 /* THEME SETUP
 ------------------------------------------------ */
