62ec53075069e0f9fa48a9f965ecfe1a5652bdf4
Lennert Slabbinck <LennertSk@users.noreply.github.com>
2019-05-31 11:56:30 +0200

    Update custom-welcome-plugin.php

--- wp-content/plugins/custom-welcome-plugin/custom-welcome-plugin.php
+++ wp-content/plugins/custom-welcome-plugin/custom-welcome-plugin.php
@@ -19,6 +19,7 @@ add_action( 'widgets_init', 'src_load_widgets' );
 
 function src_load_widgets() {
     register_widget( 'My_Custom_Widget');
+    register_widget( 'My_Employment_Widget');
 }
 
 // Widget 01 ----------------------------------------------
@@ -52,6 +53,62 @@ class My_Custom_Widget extends WP_Widget {
 	    echo $args['after_widget'];
 	}
 
+}
+
+// Widget 02
+class My_Employment_Widget extends WP_Widget {
+
+    public function __construct() {
+    // Define the constructor
+
+        $options = array(
+            'classname' => 'My_Employment_Widget',
+            'description' => 'My Employment Widget',
+        );
+
+        parent::__construct(
+            'My_Employment_Widget', 'My Employment Widget', $options
+        );
+    }
+
+    public function widget( $args, $instance ) {
+    // Keep this line
+        echo $args['before_widget'];
+
+        $workType  = $instance['work_type'];
+        echo "Current state of employment: " . $workType;
+
+        // Keep this line
+        echo $args['after_widget'];        
+    }
+
+    // Back-end form of the Widget
+    public function form( $instance ) {
+
+        if ( isset( $instance[ 'work_type' ] ) ) {
+            $workType = $instance[ 'work_type' ];
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'work_type' ); ?>">Age:</label> 
+            <select class="widefat" id="<?php echo $this->get_field_id('work_type'); ?>" name="<?php echo $this->get_field_name('work_type'); ?>">
+                <?php
+                    $options = array( 'Working', 'Student', 'Out of Work' );
+                    foreach ( $options as $option ) {
+                        echo '<option value="' . $option . '" id="' . $option . '"', $workType == $option ? ' selected="selected"' : '', '>' . $option . '</option>';
+                    }
+                ?>
+            </select>
+        </p>
+       
+    <?php 
+    }
+
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['work_type']     = ( !empty( $new_instance['work_type'] ) ) ? strip_tags( $new_instance['work_type'] ) : '';
+        return $instance;
+    }
 }
 
 
